044a22ec4d7961088e540a970ac0791997d9ac0b
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-07-31 21:54:31 +0200

    make USE_CRT_NEW_DELETE an option

--- Code/Core/Mem/Mem.cpp
+++ Code/Core/Mem/Mem.cpp
@@ -143,7 +143,7 @@ void Free( void * ptr )
     void operator delete( void * ptr, size_t /*size*/ ) noexcept { Free( ptr ); }
     void operator delete[]( void * ptr ) noexcept { Free( ptr ); }
     void operator delete[]( void * ptr, size_t /*size*/ ) noexcept { Free( ptr ); }
-#elif !defined(USE_CRT_NEW_DELETE)
+#elif !(defined(USE_CRT_NEW_DELETE) && USE_CRT_NEW_DELETE)
     #if !__has_feature( address_sanitizer ) && !__has_feature( memory_sanitizer ) && !__has_feature( thread_sanitizer ) && !defined( __SANITIZE_ADDRESS__ )
         void * operator new( size_t size ) { return Alloc( size ); }
         void * operator new[]( size_t size ) { return Alloc( size ); }
